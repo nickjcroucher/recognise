cca11bd7b6608395f95439ad1fad710bed74e0dc
Nick Croucher <n.croucher@imperial.ac.uk>
2022-09-09 06:35:36 +0100

    Add 3seq tests

--- test/run_test.py
+++ test/run_test.py
@@ -46,3 +46,24 @@ class RecogniseTests(unittest.TestCase):
                                 ' --recombinant-list ./data/three_recombinants.list --output ' + \
                                  'one_multifasta_recombinant_no_donor_test',
                                 shell=True)
+
+    def test_one_contiguous_recombinant(self):
+        subprocess.check_output('recognise --recipient ./data/AE007317.1.fasta' + \
+                                ' --recombinant-list ./data/one_recombinant.list --output ' + \
+                                ' --donor ./data/FQ312027.1.fasta --method 3seq ' + \
+                                 'one_recombinant_test',
+                                shell=True)
+
+    def test_three_contiguous_recombinants(self):
+        subprocess.check_output('recognise --recipient ./data/AE007317.1.fasta' + \
+                                ' --recombinant-list ./data/three_recombinants.list --output ' + \
+                                ' --donor ./data/FQ312027.1.fasta --method 3seq ' + \
+                                 'multiple_recombinants_test',
+                                shell=True)
+
+    def test_one_noncontiguous_recombinant(self):
+        subprocess.check_output('recognise --recipient ./data/AE007317.1.fasta' + \
+                                ' --recombinant-list ./data/three_recombinants.list --output ' + \
+                                ' --donor ./data/FQ312027.1.fasta --method 3seq ' + \
+                                 'one_multifasta_recombinant_test',
+                                shell=True)
