6fcc36b135e7aa609082fe6687d3aa8ace1bfa34
Nick Croucher <n.croucher@imperial.ac.uk>
2022-09-09 08:56:52 +0100

    Correct formatting of 3seq call

--- recognise/threeseq.py
+++ recognise/threeseq.py
@@ -27,14 +27,18 @@ def mask_alignment(alignment_file,breakpoints_3seq,recombinant_id,output_alignme
     
     return 0
 
-def get_3seq_breakpoints(aln_file,recipient_id,donor_id,recombinant_id):
+def get_3seq_breakpoints(aln_file,recipient_id,donor_id,recombinant_id,length_min,threeseq_p):
     
     # wrap in try/except because 3seq reaches a threshold at which it exits with an error code
     breakpoints = []
     try:
         output_3seq = subprocess.run('yes | 3seq -triplet ' + aln_file + ' ' + \
-                                                recipient_id + ' ' + donor_id + ' ' + recombinant_id + \
-                                                ' -L1 -id ' + recombinant_id,
+                                                recipient_id + ' ' + \
+                                                donor_id + ' ' + \
+                                                recombinant_id + \
+                                                ' -L' + str(length_min) + \
+                                                ' -t' + str(threeseq_p) + \
+                                                ' -id ' + recombinant_id,
                                                 capture_output=True,
                                                 shell = True)
         breakpoint_pattern = re.compile("([0-9]+)-([0-9]+) & ([0-9]+)-([0-9]+)")
